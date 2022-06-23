37e30deedafb78f9cccca5e3e2289a604175f988
Anderson <andeersonr@gmail.com>
2022-06-22 20:45:10 -0500

    despliegue Heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require("./src/app");
 const { connect } = require("./src/db");
 
-const port = process.env.APP_PORT;
-const host = process.env.APP_URL;
+const port = process.env.PORT || process.env.APP_PORT;
+
 connect();
 
 app.listen(port, () => {
-  console.log(`App running in ${host}:${port}`);
+  console.log(`App running in ${port}`);
 });
